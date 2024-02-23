7ea7bd2df166f51508c9e70c794a644d4cc200d2
stdcyanout <156108943+stdcyanout@users.noreply.github.com>
2024-02-23 22:51:22 +0800

    Update and rename main 2.cpp to 1648.cpp

--- CSES/1648.cpp
+++ CSES/1648.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include <bits/stdc++.h>
 #define int long long
 #define lc 2*id+1
